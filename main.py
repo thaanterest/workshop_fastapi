853bdd6803a03c92751d56bc1083a6c063761bd8
thaanterest Desktop <thaanterest@gmail.com>
2025-06-17 15:19:45 +0700

    feate: post/items

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World v2"}
+
 
 @app.get("/todos")
 async def get_todos():
@@ -14,12 +16,16 @@ async def get_todos():
         {"id": 2, "detail": "seconds todos"},
     ]
 
+
 counter = 0
+
+
 @app.get("/counter")
 async def get_counter():
     global counter
     counter += 1
-    return {"message" : f"counter = {counter}"}
+    return {"message": f"counter = {counter}"}
+
 
 class Item(BaseModel):
     item_id: int
@@ -27,23 +33,35 @@ class Item(BaseModel):
     description: str | None = None
     price: float
 
+
 class ItemDto(BaseModel):
     name: str
     description: str | None = None
     price: float
 
-items: dict[int, Item] = {}
+
+items: dict[int, Item] = {
+    1 : Item(item_id=1, name="first item",price=10),
+    2 : Item(item_id=2, name="second item",price=20),
+    3 : Item(item_id=3, name="third item",price=30)
+}
 id = 0
 
-@app.post("/items/", response_model=Item,tags=["items"])
+
+@app.post("/items/", response_model=Item, tags=["items"])
 def create_item(item: ItemDto):
     global id
     id += 1
 
     items[id] = Item(
-        item_id=id,
-        name=item.name,
-        description=item.description,
-        price=item.price
+        item_id=id, name=item.name, description=item.description, price=item.price
     )
     return items[id]
+
+@app.get("/items", response_model=list[Item], tags=["items"])
+def get_all_items():
+    return [item for item in items.values()]
+
+@app.get("/items/{item_id}", response_model=Item)
+def get_one_items(item_id: int):
+    return items[item_id]
