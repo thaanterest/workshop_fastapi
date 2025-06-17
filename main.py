4a9d2f0341093be15bce4c55aeaab29b80d5d5d8
thaanterest Desktop <thaanterest@gmail.com>
2025-06-17 15:31:24 +0700

    feate: post/items

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# workshop 006
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -45,7 +47,7 @@ items: dict[int, Item] = {
     2 : Item(item_id=2, name="second item",price=20),
     3 : Item(item_id=3, name="third item",price=30)
 }
-id = 0
+id = 3
 
 
 @app.post("/items/", response_model=Item, tags=["items"])
@@ -62,6 +64,22 @@ def create_item(item: ItemDto):
 def get_all_items():
     return [item for item in items.values()]
 
-@app.get("/items/{item_id}", response_model=Item)
+@app.get("/items/{item_id}", response_model=Item|dict, tags=["items"])
 def get_one_items(item_id: int):
+    if item_id not in items:
+        return {"message": "item not found"}
     return items[item_id]
+
+@app.put("/items/{item_id}", response_model=Item | dict, tags=["items"])
+def update_item(item_id: int, item: ItemDto):
+    if item_id not in items:
+        return {"message": "item not found"}
+    items[item_id] = Item(item_id=item_id,name=item.name,description=item.description,price=item.price)
+    return items[item_id]
+
+@app.delete("/items/{item_id}", response_model=dict, tags=["items"])
+def delete_item(item_id: int):
+    if item_id not in items:
+        return {"message": "item not found"}
+    del items[item_id]
+    return {"message": "delete success"}
