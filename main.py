5e938038bf336ff477a974fb14eb106f87509876
thaanterest Desktop <thaanterest@gmail.com>
2025-06-17 14:38:02 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI 
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/todos")
+async def get_todos():
+    return [
+        {"id" : 1, "detail": "first todos"},
+        {"id" : 2, "detail": "Seconds todos"},
+        {"id" : 3, "detail": "Third todos"}
+    ]
+
+
+counter = 0
+@app.get("/counter")
+async def get_counter():
+    global counter
+    counter += 1
+    return {"message": f"Counter is {counter}"}
