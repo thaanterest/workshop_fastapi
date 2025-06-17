de5a7499f82a7bfdf3fb8b352c69354b7f88a2f3
thaanterest Desktop <thaanterest@gmail.com>
2025-06-17 15:00:30 +0700

    feate: post/items

--- main.py
+++ main.py
@@ -1,24 +1,49 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World v2"}
 
 @app.get("/todos")
 async def get_todos():
     return [
-        {"id" : 1, "detail": "first todos"},
-        {"id" : 2, "detail": "Seconds todos"},
-        {"id" : 3, "detail": "Third todos"}
+        {"id": 1, "detail": "first todos"},
+        {"id": 2, "detail": "seconds todos"},
     ]
-
 
 counter = 0
 @app.get("/counter")
 async def get_counter():
     global counter
     counter += 1
-    return {"message": f"Counter is {counter}"}
+    return {"message" : f"counter = {counter}"}
+
+class Item(BaseModel):
+    item_id: int
+    name: str
+    description: str | None = None
+    price: float
+
+class ItemDto(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+
+items: dict[int, Item] = {}
+id = 0
+
+@app.post("/items/", response_model=Item,tags=["items"])
+def create_item(item: ItemDto):
+    global id
+    id += 1
+
+    items[id] = Item(
+        item_id=id,
+        name=item.name,
+        description=item.description,
+        price=item.price
+    )
+    return items[id]
